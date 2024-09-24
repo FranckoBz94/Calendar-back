652ad384c9a3fb714a81903c52c1436343f93fec
unknown <francoberatz.fb@gmail.com>
2024-09-24 16:02:14 -0300

    change

--- src/database/database.js
+++ src/database/database.js
@@ -1,17 +1,18 @@
-import mysql from "promise-mysql"
-import config from "../config"
+import mysql from "promise-mysql";
+import config from "../config";
 
 const connection = mysql.createConnection({
   host: config.host,
   database: config.database,
   user: config.user,
   password: config.password,
-})
+  port: config.port,
+});
 
 const getConnection = () => {
-  return connection
-}
+  return connection;
+};
 
 module.exports = {
   getConnection,
-}
+};
