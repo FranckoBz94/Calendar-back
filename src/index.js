9905c253f326a801617d553e6c2b0acd01412468
unknown <francoberatz.fb@gmail.com>
2024-11-21 20:49:13 -0300

    data socket

--- src/index.js
+++ src/index.js
@@ -8,15 +8,31 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "https://calendarfront-alpha.vercel.app", // Especifica el origen permitido
     methods: ["GET", "POST", "PUT", "DELETE"], // Los métodos permitidos
     credentials: true,
+    allowedHeaders: ["Origin", "X-Requested-With", "Content-Type", "Accept"],
   },
   connectionStateRecovery: {},
 });
 
+// Configuración de CORS para otras rutas si es necesario
+app.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://calendarfront-alpha.vercel.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 const port = app.get("port");
 console.log(port);
+
 io.on("connection", (socket) => {
   console.log("a user has connected");
 
